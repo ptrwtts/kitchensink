8a9258a46b409c801deb5d96abb25cdeafc9ac78
Peter Watts <pwattsmail@gmail.com>
2012-01-11 22:48:52 +1100

    now it's offical

--- js/kitchensink.js
+++ js/kitchensink.js
@@ -1,14 +1,40 @@
 console.log("Use the Source, Luke");
 
 // Initialize the Spotify objects
-var sp = getSpotifyApi(1);
+var sp = getSpotifyApi(1),
 	m = sp.require("sp://import/scripts/api/models"),
 	v = sp.require("sp://import/scripts/api/views"); 
 
 // I tend to leave these uncommented for fast access to the API calls
 console.log(sp.core);						
 console.log(sp.trackPlayer);
-console.log(sp.social);
+
+/*
+var myAwesomePlaylist = new m.Playlist.fromURI("spotify:user:ptrwtts:playlist:1BQMVS6c1ApNXssOmR9IzM");
+myAwesomePlaylist.name = 'My Edition Vol 1';
+myAwesomePlaylist.description = 'wefwefw';
+myAwesomePlaylist.add(m.player.track);
+myAwesomePlaylist.add("spotify:track:2xhElnF3ASgVAjCnjbzQAf");
+
+var u = m.User.fromURI("spotify:user:nlogax", function(user) {
+    console.log("User loaded", user.name);
+});
+
+var toplist = new m.Toplist();
+console.log(toplist)
+toplist.user = "nlogax";						// According to Docs, should actually be userName
+toplist.matchType = m.TOPLISTMATCHES.ARTISTS;	// Album doesn't work!
+toplist.region = "user";
+
+toplist.observe(m.EVENT.CHANGE, function() {
+	console.log(toplist.results);
+	toplist.results.forEach(function(artist) {
+		//console.log(artist.name);
+	});
+});
+
+toplist.run();
+*/
 
 
 // Handle URI arguments

5601b3e73da7541ecead9b4aaf98d2743ee0c3d0
Peter Watts <pwattsmail@gmail.com>
2012-01-12 00:55:18 +1100

    update search for new API

--- js/search.js
+++ js/search.js
@@ -20,168 +20,120 @@ $(function(){
 				case "search-basic":
 					$("#search-results").empty();
 					$("#search-results").append("<h2>Artists</h2>");
-					sp.core.search(query, true, false, {		// using "true, false" will include local results if available. no idea what the values represent!
-						onSuccess: function(response) {
-							if(response.artists.length) {
-								$.each(response.artists,function(index,artist){
-									if(index<5) {
-										$("#search-results").append('<div><a href="'+artist.uri+'">'+artist.name+'</a></div>');
-									}
-								});
-							} else {
-								$("#search-results").append('<div>No artists in results</div>');
-							}
-							$("#search-results").append("<h2>Albums</h2>");
-							if(response.albums.length) {
-								$.each(response.albums,function(index,album){
-									if(index<5) {
-										$("#search-results").append('<div><a href="'+album.uri+'">'+album.name+'</a></div>');
-									}
-								});
-							} else {
-								$("#search-results").append('<div>No albums in results</div>');
-							}
-							$("#search-results").append("<h2>Tracks</h2>");
-							if(response.tracks.length) {
-								tempPlaylist = new models.Playlist();
-								$.each(response.tracks,function(index,track){
-									if(track.uri.split(":")[1]!="local") {					// Need to exclude local tracks or the artwork fails....
-										tempPlaylist.add(m.Track.fromURI(track.uri));
-									}
-								});				
-								var playlistArt = new views.Player();
-									playlistArt.track = tempPlaylist.get(0);
-									playlistArt.context = tempPlaylist;
-									$("#search-results").append(playlistArt.node);
-								var saveButton = "<button id='savePlaylist' class='add-playlist button icon'>Save As Playlist</button>";
-									$("#search-results .sp-player").append(saveButton);
-								var playlistList = new views.List(tempPlaylist);
-									playlistList.node.classList.add("temporary");
-									$("#search-results").append(playlistList.node);
-							} else {
-								$("#search-results").append('<div>No tracks in results</div>');
-							}
-						} 
-					});
-					break;
-				case "search-play":
-					sp.core.search(query, true, false, {
-						onSuccess: function(response) {
-							if(response.tracks[0]) {
-								sp.trackPlayer.playTrackFromUri(response.tracks[0].uri, { onSuccess: function() {} });
-							}
+					var search = new models.Search(query);
+					search.localResults = models.LOCALSEARCHRESULTS.APPEND;		// Local files last
+					search.observe(models.EVENT.CHANGE, function() {
+						if(search.artists.length) {
+							$.each(search.artists,function(index,artist){
+								if(index<5) {
+									$("#search-results").append('<div><a href="'+artist.uri+'">'+artist.name+'</a></div>');
+								}
+							});
+						} else {
+							$("#search-results").append('<div>No artists in results</div>');
+						}
+						$("#search-results").append("<h2>Albums</h2>");
+						if(search.albums.length) {
+							$.each(search.albums,function(index,album){
+								if(index<5) {
+									$("#search-results").append('<div><a href="'+album.uri+'">'+album.name+'</a></div>');
+								}
+							});
+						} else {
+							$("#search-results").append('<div>No albums in results</div>');
+						}
+						$("#search-results").append("<h2>Tracks</h2>");
+						if(search.tracks.length) {
+							tempPlaylist = new models.Playlist();
+							$.each(search.tracks,function(index,track){
+								tempPlaylist.add(models.Track.fromURI(track.uri));				// Note: artwork is compiled from first few tracks. if any are local it will fail to generate....
+							});				
+							var playlistArt = new views.Player();
+								playlistArt.track = tempPlaylist.get(0);
+								playlistArt.context = tempPlaylist;
+								$("#search-results").append(playlistArt.node);
+							var saveButton = "<button id='savePlaylist' class='add-playlist button icon'>Save As Playlist</button>";
+								$("#search-results .sp-player").append(saveButton);
+							var playlistList = new views.List(tempPlaylist);
+								playlistList.node.classList.add("temporary");
+								$("#search-results").append(playlistList.node);
+						} else {
+							$("#search-results").append('<div>No tracks in results</div>');
 						}
 					});
+					search.appendNext();
+					break;
+				case "search-play":
+					var search = new models.Search(query);
+					search.localResults = models.LOCALSEARCHRESULTS.PREPEND;		// Local files first
+					search.observe(models.EVENT.CHANGE, function() {
+						if(search.tracks.length) {
+							player.play(search.tracks[0].uri);
+						}
+					});
+					search.appendNext();
 				break;
 				case "search-explore":
 					$("#search-results").html('Check the console (right-click > show inspector) to see data');
-					console.log("** This is a demo to show the different ways to get info on artists / albums / tracks **");
+					console.log("** This compares local / remote information returned for artists / albums / tracks **");
 					console.log("** It scans the first artist / album / track found in a search request **");
 					console.log("** It is also a demonstration of handling asynchronous calls **");
 					console.log("** All results will be placed in the object below as they come back **");
 					var results = {}
 					console.log(results);
 					asyncCalls = []; 	//prepare for async calls
-					sp.core.search(query, true, true, {		// using "true, true" excludes local results
-						onSuccess: function(response) {
-							// ARTIST
-							if(response.artists.length) {
-								results.artist = {};
-								var artist = response.artists[0];
-								// Artist Object		
-									results.artist["Create an Artist Object"] = models.Artist.fromURI(artist.uri);
-								// Browse URI
-									asyncCalls.push("artistBrowseUri");
-									sp.core.browseUri(artist.uri, {
-										onSuccess: function(response) {
-											asyncComplete("artistBrowseUri");
-											results.artist["Browse an Artist URI"] = response;
-											// Artist Tracks
-											asyncCalls.push("artistTracks");
-											sp.core.getMetadata(response.playlist.all(), {
-												onSuccess: function(tracks) {
-													asyncComplete("artistTracks");
-													results.artist["Get all tracks for an Artist"] = {"tracks":tracks};
-												}
-											});
-										}
-									});
-								// Get Metadata (local)
-									asyncCalls.push("artistLocalMetadata");
-									sp.core.getMetadata(artist.uri, {
-										onSuccess: function(response) {
-											asyncComplete("artistLocalMetadata");
-											results.artist["Get Local Metadata for an Artist"] = response;
-										}
-									});
-								// Get Metadata (remote)
-									asyncCalls.push("artistRemoteMetadata");
-									$.ajax({
-										url: "http://ws.spotify.com/lookup/1/.json?uri="+artist.uri+"&extras=albumdetail",
-										success: function(response) {
-											asyncComplete("artistRemoteMetadata");
-											results.artist["Get Remote Metadata for an Artist"] = response;
-										}
-								  	});								
-							}
-							// ALBUM
-							if(response.albums.length) {
-								results.album = {};
-								var album = response.albums[0];
-								// Album Object					
-									results.album["Create an Album Object"] = models.Album.fromURI(album.uri);
-								// Browse URI
-									asyncCalls.push("albumBrowseUri");
-									sp.core.browseUri(album.uri, {
-										onSuccess: function(response) {
-											asyncComplete("albumBrowseUri");
-											results.album["Browse an Album URI"] = response;
-										}
-									});
-								// Get Metadata (local)
-									asyncCalls.push("albumLocalMetadata");
-									sp.core.getMetadata(album.uri, {
-										onSuccess: function(response) {
-											asyncComplete("albumLocalMetadata");
-											results.album["Get Local Metadata for an Album"] = response;
-										}
-									});
-								// Get Metadata (remote)
-									asyncCalls.push("albumRemoteMetadata");
-									$.ajax({
-										url: "http://ws.spotify.com/lookup/1/.json?uri="+album.uri+"&extras=trackdetail",
-										success: function(response) {
-											asyncComplete("albumRemoteMetadata");
-											results.album["Get Remote Metadata for an Album"] = response;
-										}
-								  	});								
-							}		
-							// TRACK
-							if(response.tracks.length) {
-								results.track = {};
-								var track = response.tracks[0];
-								// Track Object					
-									results.track["Create a Track Object"] = models.Track.fromURI(track.uri);
-								// Get Metadata (local)
-									asyncCalls.push("trackLocalMetadata");
-									sp.core.getMetadata(track.uri, {
-										onSuccess: function(response) {
-											asyncComplete("trackLocalMetadata");
-											results.track["Get Local Metadata for a Track"] = response;
-										}
-									});
-								// Get Metadata (remote)
-									asyncCalls.push("trackRemoteMetadata");
-									$.ajax({
-										url: "http://ws.spotify.com/lookup/1/.json?uri="+track.uri,
-										success: function(response) {
-											asyncComplete("trackRemoteMetadata");
-											results.track["Get Remote Metadata for a Track"] = response;
-										}
-								  	});								
-							}							
+					var search = new models.Search(query);
+					search.observe(models.EVENT.CHANGE, function() {
+						// ARTIST
+						if(search.artists.length) {
+							results.artist = {};
+							var artist = search.artists[0];
+							// Artist Object (local)	
+								results.artist["Create an Artist Object"] = models.Artist.fromURI(artist.uri);
+							// Get Metadata (remote)
+								asyncCalls.push("artistRemoteMetadata");
+								$.ajax({
+									url: "http://ws.spotify.com/lookup/1/.json?uri="+artist.uri+"&extras=albumdetail",
+									success: function(response) {
+										asyncComplete("artistRemoteMetadata");
+										results.artist["Get Remote Metadata for an Artist"] = response;
+									}
+							  	});								
 						}
-					});
+						// ALBUM
+						if(search.albums.length) {
+							results.album = {};
+							var album = search.albums[0];
+							// Album Object (local)		
+								results.album["Create an Album Object"] = models.Album.fromURI(album.uri);
+							// Get Metadata (remote)
+								asyncCalls.push("albumRemoteMetadata");
+								$.ajax({
+									url: "http://ws.spotify.com/lookup/1/.json?uri="+album.uri+"&extras=trackdetail",
+									success: function(response) {
+										asyncComplete("albumRemoteMetadata");
+										results.album["Get Remote Metadata for an Album"] = response;
+									}
+							  	});								
+						}		
+						// TRACK
+						if(search.tracks.length) {
+							results.track = {};
+							var track = search.tracks[0];
+							// Track Object	 (local)				
+								results.track["Create a Track Object"] = models.Track.fromURI(track.uri);
+							// Get Metadata (remote)
+								asyncCalls.push("trackRemoteMetadata");
+								$.ajax({
+									url: "http://ws.spotify.com/lookup/1/.json?uri="+track.uri,
+									success: function(response) {
+										asyncComplete("trackRemoteMetadata");
+										results.track["Get Remote Metadata for a Track"] = response;
+									}
+							  	});								
+						}
+					});					
+					search.appendNext();
 				break;
 			}
 		}
